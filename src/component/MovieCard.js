2ab855edcb1dc51a5777cb2f3be48d2e60f182ee
Vishal Kumar <5060vishalkumar@gmail.com>
2023-01-03 10:32:54 +0530

    version 1

--- src/component/MovieCard.js
+++ src/component/MovieCard.js
@@ -9,7 +9,9 @@ function MovieCard() {
             {Data.map((value, index) => {
               return (
                 <div className="lg:w-1/4 md:w-1/2 p-4 w-full drop-shadow-md" key={index}>
-                  <img alt="ecommerce" className="object-cover object-center w-full h-full block" src={value.Poster} />
+                  <a href="/githubreact" className="block relative h-96 rounded overflow-hidden">
+                    <img alt="ecommerce" className="object-cover object-center w-full h-full block" src={value.Poster} />
+                  </a>
                   <div className="mt-4">
                     <h3 className="text-gray-100 text-xs tracking-widest title-font mb-1 text-center">{value.Type}</h3>
                     <p className='mt-1 text-center text-white'>{value.Year}</p>
